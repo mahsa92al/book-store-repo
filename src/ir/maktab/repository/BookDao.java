ff2d0739b975034827e2ea6da2b4fc39243a30a8
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 22:23:54 +0330

    added findBooksInfo to BookDao

--- src/ir/maktab/repository/BookDao.java
+++ src/ir/maktab/repository/BookDao.java
@@ -3,7 +3,10 @@ package ir.maktab.repository;
 import ir.maktab.model.Book;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -17,7 +20,21 @@ public class BookDao extends BaseDao{
         connection = getConnection();
     }
 
-    public Map<String, List<Book>> findBooksInfo() {
-        return null;
+    public List<Book> findBooksInfo() throws SQLException {
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from book_store");
+        List<Book> books = new ArrayList<>();
+        while (resultSet.next()){
+            Book book = new Book();
+            book.setId(resultSet.getInt("id"));
+            book.setAuthorName(resultSet.getString("author_name"));
+            book.setIsbn(resultSet.getString("isbn"));
+            book.setBookTitle(resultSet.getString("book_title"));
+            book.setPublishedYear(resultSet.getInt("published_year"));
+            book.setPrice(resultSet.getInt("price"));
+            book.setSoldNumber(resultSet.getInt("sold_number"));
+            books.add(book);
+        }
+        return books;
     }
 }
