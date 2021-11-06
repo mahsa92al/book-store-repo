ebb08db8f4aef51a7057787be5fae76cd61b950e
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 20:50:29 +0330

    added BookDao constructor to BookDao

--- src/ir/maktab/repository/BookDao.java
+++ src/ir/maktab/repository/BookDao.java
@@ -1,7 +1,18 @@
 package ir.maktab.repository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 /**
  * @author Mahsa Alikhani m-58
  */
-public class BookDao {
+public class BookDao extends BaseDao{
+    private final Connection connection;
+
+    public BookDao() throws SQLException, ClassNotFoundException {
+        connection = getConnection();
+    }
+
+    public void updateAllBooksSoldTotalPrice() {
+    }
 }
