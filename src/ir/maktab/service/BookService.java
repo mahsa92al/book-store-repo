429a9cadee31f27b65a55a09a9ebbd902e050df7
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 20:53:15 +0330

    added throws SQLException and ClassNotFoundException to BookService constructor

--- src/ir/maktab/service/BookService.java
+++ src/ir/maktab/service/BookService.java
@@ -1,6 +1,10 @@
 package ir.maktab.service;
 
+import ir.maktab.model.Book;
 import ir.maktab.repository.BookDao;
+
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -8,11 +12,11 @@ import ir.maktab.repository.BookDao;
 public class BookService {
     private final BookDao bookDao;
 
-    public BookService() {
+    public BookService() throws SQLException, ClassNotFoundException {
         bookDao = new BookDao();
     }
 
     public void calculateAllBooksSoldTotalPrice() {
-
+        bookDao.updateAllBooksSoldTotalPrice();
     }
 }
