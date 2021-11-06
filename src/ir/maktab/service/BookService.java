4f2a69b3c946d7b1f20b3a59ec99fb10e3dfc87a
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 17:53:45 +0330

    added initialized  bookDao final variable

--- src/ir/maktab/service/BookService.java
+++ src/ir/maktab/service/BookService.java
@@ -1,9 +1,18 @@
 package ir.maktab.service;
 
+import ir.maktab.repository.BookDao;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class BookService {
+    private final BookDao bookDao;
+
+    public BookService() {
+        bookDao = new BookDao();
+    }
+
     public void calculateAllBooksSoldTotalPrice() {
+
     }
 }
