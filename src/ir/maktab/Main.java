753559bc6873cb95af20c16080d15e58a84ec583
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 17:30:10 +0330

    added management menu to Main

--- src/ir/maktab/Main.java
+++ src/ir/maktab/Main.java
@@ -1,10 +1,41 @@
 package ir.maktab;
 
+import ir.maktab.service.BookService;
+
+import java.util.Scanner;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class Main {
-    public static void main(String[] args) {
+    private static final Scanner scanner = new Scanner(System.in);
+    private static BookService bookService;
 
+    public static void main(String[] args) {
+        bookService = new BookService();
+        int choiceNumber;
+        do{
+            System.out.println("***Management Menu***");
+            System.out.println("1. View books information report\n2. Exit");
+            String choice;
+            do {
+                choice = scanner.next();
+            }while (!isNumeric(choice));
+            choiceNumber = Integer.parseInt(choice);
+            switch (choiceNumber){
+                case 1:
+                    viewBooksInfo();
+                    break;
+                case 2:
+                    break;
+            }
+        }while (choiceNumber != 2);
+    }
+
+    private static void viewBooksInfo() {
+    }
+
+    private static boolean isNumeric(String choice) {
+        return choice.matches("[1-9]+");
     }
 }
