636d798f348939f29d108cba5e84d2801c5ffb7f
Mahsa <mahsa.alikhany@gmail.com>
2021-11-06 12:43:08 +0330

    added getConnection to BaseDao

--- src/ir/maktab/repository/BaseDao.java
+++ src/ir/maktab/repository/BaseDao.java
@@ -1,7 +1,24 @@
 package ir.maktab.repository;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * @author Mahsa Alikhani m-58
  */
 public class BaseDao {
+    private Connection connection;
+
+    public BaseDao(Connection connection) {
+        this.connection = connection;
+    }
+
+    public Connection getConnection() throws ClassNotFoundException, SQLException {
+        if (connection == null){
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/book_store", "root", "5103583");
+        }
+        return connection;
+    }
 }
