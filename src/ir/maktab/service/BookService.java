599461c84e0dec1bbf646bae7e798582a9ae15c5
Mahsa <mahsa.alikhany@gmail.com>
2021-11-12 00:37:05 +0330

    added getBooksInfo to BookService

--- src/ir/maktab/service/BookService.java
+++ src/ir/maktab/service/BookService.java
@@ -4,8 +4,10 @@ import ir.maktab.model.Book;
 import ir.maktab.repository.BookDao;
 
 import java.sql.SQLException;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 /**
  * @author Mahsa Alikhani m-58
@@ -17,7 +19,13 @@ public class BookService {
         bookDao = new BookDao();
     }
 
-    public void getBooksInfo() {
-        Map<String, List<Book>> daoBooksInfo = bookDao.findBooksInfo();
+    public Map<String, List<Book>> getBooksInfo() throws SQLException {
+        List<Book> booksInfo = bookDao.findBooksInfo();
+        booksInfo.forEach(i-> i.setSoldTotalPrice(i.getSoldNumber() * i.getPrice()));
+        Map<String, List<Book>> sorted = booksInfo.stream()
+                .sorted(Comparator.comparing(Book::getAuthorName)
+                        .thenComparing(Comparator.comparing(Book::getPublishedYear).reversed()))
+                .collect(Collectors.groupingBy(Book::getAuthorName, LinkedHashMap::new, Collectors.toList()));
+        return sorted;
     }
 }
